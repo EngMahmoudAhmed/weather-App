2ead7bb61077de3a739fb2697c3029106a87c710
Mahmoud-Ahmed <lya440676@gmail.com>
2023-09-19 15:11:20 +0300

    new commit

--- src/components/SearchWeather/Searchweather.jsx
+++ src/components/SearchWeather/Searchweather.jsx
@@ -2,24 +2,30 @@ import React, { useEffect, useState } from 'react'
 import image1 from '../assets/image1.jpg'
 
 const Searchweather = () => {
+    const [search, setSearch] = useState('London');
     const [data, setData] = useState([]);
     const [input, setInput] = useState('');
-    const [search, setSearch] = useState('London');
-    let combonentMonted =true;
+    let componentMonted = true;
 
     useEffect(() => {
         const fetchWeather = async () => {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=8bc69c7d20c98ec4320b02ca2c334a54`)
-            if (combonentMonted) {
-                setData(await response.json())
-                console.log(data)
+            if (componentMonted) {
+                setData((await response.json()))
+                console.log(response);
+                console.log(data);
+
             }
             return () => {
-                combonentMonted = false
+                componentMonted = false
             }
         }
+
         fetchWeather();
     }, []);
+
+
+    let temp = (data.main.temp - 273.15).toFixed(2)
 
     return (
         <div>
@@ -31,19 +37,22 @@ const Searchweather = () => {
                             <div className="card-img-overlay">
                                 <form>
                                     <div className="input-group mb-4 w-75 mx-auto">
-                                        <input type="search" className="form-control" placeholder="Seach City" aria-label="Search City" aria-describedby="basic-addon2" />
+                                        <input type="search" onChange={(e) => {
+                                            setInput(e.target.value)
+                                        }} className="form-control" placeholder="Seach City" aria-label="Search City" aria-describedby="basic-addon2" />
                                         <button type='submit' className="input-group-text" id="basic-addon2">
                                             <i className="fas fa-search"></i>
                                         </button>
                                     </div>
+                                        <h3>{ input}</h3>
                                 </form>
                                 <div className="bg-dark bg-opacity-50 text-white">
                                     <h2 className="card-title py-3">{data.name}</h2>
                                     <p className="card-text lead">Thursday, Septemper 19, 2023</p>
                                     <hr />
                                     <i className="fas fa-cloud fa-4x"></i>
-                                    <h1 className='fw-bolder mb-5'>33.06&deg;C</h1>
-                                    <p className='fw-bolder lead mb-0'>Cloud</p>
+                                    <h1 className='fw-bolder mb-5'>{temp }&deg;C</h1>
+                                    <p className='fw-bolder lea mb-0'>Cloud</p>
                                     <p className='lead'>30.01&deg;C | 35.01&deg;C</p>
                                 </div>
                             </div>
