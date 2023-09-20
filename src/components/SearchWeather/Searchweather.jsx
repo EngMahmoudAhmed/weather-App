870c20b4af0bfd10fa5495ca5acaa2715f277aea
Mahmoud-Ahmed <lya440676@gmail.com>
2023-09-21 00:55:15 +0300

    new change

--- src/components/SearchWeather/Searchweather.jsx
+++ src/components/SearchWeather/Searchweather.jsx
@@ -5,6 +5,7 @@ const Searchweather = () => {
     const [search, setSearch] = useState('tokyo');
     const [data, setData] = useState([]);
     const [input, setInput] = useState('');
+    const [unsplashApi, setUnsplashApi] = useState([])
     let componentMonted = true;
 
     useEffect(() => {
@@ -22,108 +23,117 @@ const Searchweather = () => {
         fetchWeather();
     }, [search]);
 
+    useEffect(() => {
+        const unsplash_Api = async () => {
+            const response_2 = await fetch(`https://api.unsplash.com/photos/?client_id=rl1w01G1yNj20X17q4YuDCudXAN8T64jN6FicS9925I`)
+                setUnsplashApi(await response_2.json())
+            const self = (unsplashApi[0]?.links?.self);
+        }
+
+        unsplash_Api();
+    },[])
 
 
+console.log(self);
 
-    console.log(data);
 
-    if (data && data.weather && data.weather[0] && data.weather[0].main) {
-        var main = data.weather[0].main;
-        // Rest of your code here
-    } else {
-        // Handle the case when data or its properties are not defined
-        // or when main is null or undefined
+if (data && data.weather && data.weather[0] && data.weather[0].main) {
+    var main = data.weather[0].main;
+    // Rest of your code here
+} else {
+    // Handle the case when data or its properties are not defined
+    // or when main is null or undefined
+}
+
+
+let emoji = null;
+
+if (typeof data.main != 'undefined') {
+    if (data.weather[0].main == 'Clouds') {
+        emoji = 'fa-cloud'
     }
-
-
-    let emoji = null;
-
-    if (typeof data.main != 'undefined') {
-        if (data.weather[0].main == 'Clouds') {
-            emoji = 'fa-cloud'
-        }
-        else if (data.weather[0].main == 'Rain') {
-            emoji = 'fa-cloud-rain'
-        }
-        else if (data.weather[0].main == 'Thunderstorm') {
-            emoji = 'fa-bolt'
-        }
-        else if (data.weather[0].main == 'Drizzle') {
-            emoji = 'fa-cloud-rain'
-        }
-        else if (data.weather[0].main == 'Snow') {
-            emoji = 'fa-snow-flake'
-        }
-        else {
-            emoji = 'fa-smog'
-        }
+    else if (data.weather[0].main == 'Rain') {
+        emoji = 'fa-cloud-rain'
+    }
+    else if (data.weather[0].main == 'Thunderstorm') {
+        emoji = 'fa-bolt'
+    }
+    else if (data.weather[0].main == 'Drizzle') {
+        emoji = 'fa-cloud-rain'
+    }
+    else if (data.weather[0].main == 'Snow') {
+        emoji = 'fa-snow-flake'
     }
     else {
-        return (
-            <div>...Loading</div>
-        )
+        emoji = 'fa-smog'
     }
-
-    // let main= data?.weather[0]?.main;
-    let temp = (data?.main?.temp - 273.15).toFixed(2)
-    let temp_max = (data?.main?.temp_max - 273.15).toFixed(2)
-    let temp_min = (data?.main?.temp_min - 273.15).toFixed(2)
-
-
-    // date
-    let d = new Date();
-    let date = d.getDate();
-    let year = d.getFullYear();
-    let month = d.toLocaleString('default', { month: 'long' });
-    let day = d.toLocaleString('default', { weekday: 'long' });
-
-    // time
-    let time = d.toLocaleString([], {
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit'
-    });
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setSearch(input)
-    }
+}
+else {
     return (
+        <div>...Loading</div>
+    )
+}
 
-        <div>
-            <div className="container mt-5">
-                <div className="row justify-content-center">
-                    <div className="col-sm-7 col-md-6 col-lg-5">
-                        <div className="card text-white text-center border-0">
-                            <img src={image1} className="card-img" height={500} alt="..." />
-                            <div className="card-img-overlay">
-                                <form onSubmit={handleSubmit}>
-                                    <div className="input-group mb-4 w-75 mx-auto">
-                                        <input type="search" className="form-control" value={input} name='search'
-                                            placeholder="Seach City" required aria-label="Search City" aria-describedby="basic-addon2"
-                                            onChange={(e) =>  setInput(e.target.value) } />
-                                        <button type='submit' className="input-group-text" id="basic-addon2">
-                                            <i className="fas fa-search"></i>
-                                        </button>
-                                    </div>
-                                </form>
-                                <div className="bg-dark bg-opacity-50 text-white">
-                                    <h2 className="card-title py-3">{data.name}</h2>
-                                    <p className="card-text lead fw-bold">{day}, {month} {date}, {year} </p>
-                                    <h4>{time}</h4>
-                                    <hr />
-                                    <i className={`fas ${emoji} fa-4x`}></i>
-                                    <h1 className='fw-bolder mb-2'>{temp}&deg;C</h1>
-                                    <p className='fw-bolder lead mb-0'>{main}</p>
-                                    <p className='lead pb-5'>{temp_max}&deg;C | {temp_min}&deg;C</p>
+// let main= data?.weather[0]?.main;
+let temp = (data?.main?.temp - 273.15).toFixed(2)
+let temp_max = (data?.main?.temp_max - 273.15).toFixed(2)
+let temp_min = (data?.main?.temp_min - 273.15).toFixed(2)
+
+
+// date
+let d = new Date();
+let date = d.getDate();
+let year = d.getFullYear();
+let month = d.toLocaleString('default', { month: 'long' });
+let day = d.toLocaleString('default', { weekday: 'long' });
+
+// time
+let time = d.toLocaleString([], {
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit'
+});
+
+const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input)
+}
+return (
+
+    <div>
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-sm-7 col-md-6 col-lg-5">
+                    <div className="card text-white text-center border-0">
+                        <img src={self} className="card-img" height={500} alt="..." />
+                        <div className="card-img-overlay">
+                            <form onSubmit={handleSubmit}>
+                                <div className="input-group mb-4 w-75 mx-auto">
+                                    <input type="search" className="form-control" value={input} name='search'
+                                        placeholder="Seach City" required aria-label="Search City" aria-describedby="basic-addon2"
+                                        onChange={(e) => setInput(e.target.value)} />
+                                    <button type='submit' className="input-group-text" id="basic-addon2">
+                                        <i className="fas fa-search"></i>
+                                    </button>
                                 </div>
+                            </form>
+                            <div className="bg-dark bg-opacity-50 text-white">
+                                <h2 className="card-title py-3">{data.name}</h2>
+                                <p className="card-text lead fw-bold">{day}, {month} {date}, {year} </p>
+                                <h4>{time}</h4>
+                                <hr />
+                                <i className={`fas ${emoji} fa-4x`}></i>
+                                <h1 className='fw-bolder mb-2'>{temp}&deg;C</h1>
+                                <p className='fw-bolder lead mb-0'>{main}</p>
+                                <p className='lead pb-5'>{temp_max}&deg;C | {temp_min}&deg;C</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    </div>
+)
 }
 
 export default Searchweather
