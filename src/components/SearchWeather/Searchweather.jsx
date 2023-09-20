6074e54705e35c050dde0c92d00f46c1fcbeb6bb
Mahmoud-Ahmed <lya440676@gmail.com>
2023-09-20 22:40:10 +0300

    new change

--- src/components/SearchWeather/Searchweather.jsx
+++ src/components/SearchWeather/Searchweather.jsx
@@ -10,50 +10,107 @@ const Searchweather = () => {
     useEffect(() => {
         const fetchWeather = async () => {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=8bc69c7d20c98ec4320b02ca2c334a54`)
-            if (componentMonted) {
-                setData((await response.json()))
-                console.log(response);
-                console.log(data);
+            setData(await response.json())
+            // if (componentMonted) {
+            // }
 
-            }
-            return () => {
-                componentMonted = false
-            }
+            // return () => {
+            //     componentMonted = false
+            // }
         }
 
         fetchWeather();
     }, []);
 
 
-    let temp = (data.main.temp - 273.15).toFixed(2)
 
+
+    console.log(data);
+    
+    if (data && data.weather && data.weather[0] && data.weather[0].main) {
+        var main = data.weather[0].main;
+        // Rest of your code here
+    } else {
+        // Handle the case when data or its properties are not defined
+        // or when main is null or undefined
+    }
+    
+
+    let emoji = null;
+
+    if (typeof data.main != 'undefined') {
+        if (data.weather[0].main == 'Clouds') {
+            emoji = 'fa-cloud'
+        }
+        else if (data.weather[0].main == 'Rain') {
+            emoji = 'fa-cloud-rain'
+        }
+        else if (data.weather[0].main == 'Thunderstorm') {
+            emoji = 'fa-bolt'
+        }
+        else if (data.weather[0].main == 'Drizzle') {
+            emoji = 'fa-cloud-rain'
+        }
+        else if (data.weather[0].main == 'Snow') {
+            emoji = 'fa-snow-flake'
+        }
+        else {
+            emoji = 'fa-smog'
+        }
+    }
+    else {
+        return (
+            <div>...Loading</div>
+        )
+    }
+    
+    // let main= data?.weather[0]?.main;
+    let temp = (data?.main?.temp - 273.15).toFixed(2)
+    let temp_max = (data?.main?.temp_max - 273.15).toFixed(2)
+    let temp_min = (data?.main?.temp_min - 273.15).toFixed(2)
+
+
+    // date
+    let d = new Date();
+    let date = d.getDate();
+    let year = d.getFullYear();
+    let month = d.toLocaleString('default',{month:'long'});
+    let day = d.toLocaleString('default', { weekday: 'long' });
+
+    // time
+    let time = d.toLocaleString([], {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
     return (
+        
         <div>
             <div className="container mt-5">
                 <div className="row justify-content-center">
                     <div className="col-md-4">
                         <div className="card text-white text-center border-0">
-                            <img src={image1} className="card-img" height={500} alt="..." />
+                            <img src={image1} className="card-img" height={600} alt="..." />
                             <div className="card-img-overlay">
                                 <form>
                                     <div className="input-group mb-4 w-75 mx-auto">
-                                        <input type="search" onChange={(e) => {
-                                            setInput(e.target.value)
-                                        }} className="form-control" placeholder="Seach City" aria-label="Search City" aria-describedby="basic-addon2" />
+                                        <input type="search" className="form-control" value={input} name='search'
+                                            placeholder="Seach City" aria-label="Search City" aria-describedby="basic-addon2"
+                                        onChange={(e)=>{setInput(e.target.value)}}/>
                                         <button type='submit' className="input-group-text" id="basic-addon2">
                                             <i className="fas fa-search"></i>
                                         </button>
                                     </div>
-                                        <h3>{ input}</h3>
                                 </form>
                                 <div className="bg-dark bg-opacity-50 text-white">
                                     <h2 className="card-title py-3">{data.name}</h2>
-                                    <p className="card-text lead">Thursday, Septemper 19, 2023</p>
+                                    <p className="card-text lead">{day}, {month} {date}, {year} </p>
+                                    {time}
                                     <hr />
-                                    <i className="fas fa-cloud fa-4x"></i>
-                                    <h1 className='fw-bolder mb-5'>{temp }&deg;C</h1>
-                                    <p className='fw-bolder lea mb-0'>Cloud</p>
-                                    <p className='lead'>30.01&deg;C | 35.01&deg;C</p>
+                                    <i className={`fas ${emoji} fa-4x`}></i>
+                                    <h1 className='fw-bolder mb-5'>{temp}&deg;C</h1>
+                                    <p className='fw-bolder lea mb-0'>{main}</p>
+                                    <p className='lead pb-5'>{temp_max}&deg;C | {temp_min}&deg;C</p>
                                 </div>
                             </div>
                         </div>
