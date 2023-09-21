c3a3a521f32c8e6f9e29a5603e087a846cb52316
Mahmoud-Ahmed <lya440676@gmail.com>
2023-09-21 23:58:57 +0300

    delete pexels pakage

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import 'pexels'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
