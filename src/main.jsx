6f1938c8729f95e7a45529dc65c0a035f9c2979a
Mahmoud-Ahmed <lya440676@gmail.com>
2023-09-21 21:24:57 +0300

    delete index.css

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'pexels'
 import App from './App.jsx'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
